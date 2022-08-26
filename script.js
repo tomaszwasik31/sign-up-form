5e5e60b13a5ba43d2d6b722a516884599f1e8d1d
Tomasz Wasik <tomaszwasik31@gmail.com>
2022-08-26 14:45:45 +0100

    password validation added

---
+++ script.js
@@ -0,0 +1,18 @@
+let password = document.getElementById("password");
+let confirm_password = document.getElementById("confirm_password");
+
+let error_msg = document.getElementById("error_msg");
+
+function validatePassword() {
+  if (password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+    confirm_password.style="outline-color:red"
+    password.style="border-color:red"
+    error_msg.style="visibility: visible;"
+  } else {
+    confirm_password.setCustomValidity("");
+  }
+}
+
+
+confirm_password.onkeyup = validatePassword;
